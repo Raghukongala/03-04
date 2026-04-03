3b480072995bd32f3c8d7e17b466086b431fa179
Raghukongala <kongalaraghu406@gmail.com>
2026-04-03 12:28:13 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -78,7 +78,7 @@ resource "aws_ecs_service" "service" {
   name            = "devops-service"
   cluster         = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.task.arn
-  desired_count   = 10
+  desired_count   = 1
   launch_type     = "FARGATE"
 
   network_configuration {
